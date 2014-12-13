24bb464112708056cd4a1265a5fed7eeed6728e0
adolenc <andrej.dolenc@student.uni-lj.si>
2014-12-13 18:43:59 +0100

    Added handling of words that appear in both lists.
    Words that appear as both positive and negative are moved into
    `duplicates.txt' file and are removed from separate lists.

--- lexicon/translating/run_translate.sh
+++ lexicon/translating/run_translate.sh
@@ -4,8 +4,8 @@
 # eg ./run_translate.sh slownet-2014-07-08.xml positive-words.txt negative-words.txt
 
 # remove comments and redundadnd whitespace from opinion lexicon
-(grep -iv '^;' positive-words.txt | grep -iv '^$') > positive.txt
-(grep -iv '^;' negative-words.txt | grep -iv '^$') > negative.txt 
+(grep -ie '^[a-zA-Z]' positive-words.txt) > positive.txt
+(grep -ie '^[a-zA-Z]' negative-words.txt) > negative.txt 
 # translate lexicon with translate.rb
 ruby translate.rb slownet-2014-07-08.xml positive.txt negative.txt
 
@@ -13,5 +13,10 @@ ruby translate.rb slownet-2014-07-08.xml positive.txt negative.txt
 (cat tr-positive-tmp.txt| tr ' ' '\n' | grep -v '^$' | sort | uniq) > tr-positive-words.txt
 (cat tr-negative-tmp.txt | tr ' ' '\n' | grep -v '^$' | sort | uniq) > tr-negative-words.txt
 
+# move words that appear in both lists into separate file
+sort tr-positive-words.txt tr-negative-words.txt | uniq -d > duplicates.txt
+comm -23 tr-positive-words.txt duplicates.txt > positive-words.txt
+comm -23 tr-negative-words.txt duplicates.txt > negative-words.txt
+
 # remove tmp files
-# rm positive.txt negative.txt tr-positive-tmp.txt tr-negative-tmp.txt
+# rm positive.txt negative.txt tr-positive-tmp.txt tr-negative-tmp.txt tr-positive-words.txt tr-negative-words.txt
